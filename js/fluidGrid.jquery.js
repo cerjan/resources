96bc8b235fa64f9a58b86898529fd8d27fc0d465
cerjan <35720287+cerjan@users.noreply.github.com>
2018-09-17 10:16:03 +0200

    Create fluidGrid.jquery.js

---
+++ js/fluidGrid.jquery.js
@@ -0,0 +1,21 @@
+(function($) {
+	$.fn.fluidGrid = function(o) {
+		opt = $.extend({
+			basis: 240,
+			fillLastLine: false
+		}, o);
+		this.imagesLoaded()
+				.progress(function(imstance, image) {
+					div = $(image.img).parent();
+					div.css({
+						flexGrow: 2 * image.img.naturalWidth / image.img.naturalHeight,
+						flexBasis: opt.basis * image.img.naturalWidth / image.img.naturalHeight
+					});
+					$('<div class="spacer" />').css({
+						paddingTop: (100 * image.img.naturalHeight / image.img.naturalWidth) + '%'
+					}).appendTo(div);
+		});
+		if (!opt.fillLastLine) $('<div />').css({flexGrow: 20}).appendTo(this);
+		return this;
+	};
+}(jQuery));
