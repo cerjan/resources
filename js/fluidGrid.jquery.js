6811dbe646e69e5a5116947bf6a123050fcefa7f
cerjan <35720287+cerjan@users.noreply.github.com>
2019-03-05 14:05:35 +0100

    Update fluidGrid.jquery.js

--- js/fluidGrid.jquery.js
+++ js/fluidGrid.jquery.js
@@ -3,7 +3,8 @@
 		opt = $.extend({
 			basis: 240,
 			fillLastLine: false,
-			random: false
+			random: false,
+			margin: 5
 		}, o);
 		opt.percent = false;
 		if (String(opt.basis).substr(-1) === '%') {
@@ -14,10 +15,25 @@
 			console.log('Function imagesLoaded() does not exist. (use https://cerjan.github.io/resources/js/imagesloaded.pkgd.min.js)');
 			return;
 		}
-		this.find('> div').addClass('grid_item');
+		this.css({
+			display: 'flex',
+			justifyContent: 'center',
+			flexWrap: 'wrap',
+			margin: '-' + opt.margin + 'px'
+		});
+		this.find('> div').addClass('grid_item').css({
+			position: 'relative',
+			margin: opt.margin + 'px'
+		});
 		this.imagesLoaded()
 				.progress(function(instance, image) {
-					div = $(image.img).closest('.grid_item');
+					div = $(image.img).css({
+						position: 'absolute',
+						left: 0,
+						top: 0,
+						width: '100%',
+						height: '100%'
+					}).closest('.grid_item');
 					div.css({
 						flexGrow: 2 * image.img.naturalWidth / image.img.naturalHeight,
 						flexBasis: opt.basis * image.img.naturalWidth / image.img.naturalHeight + (opt.percent ? '%' : 0)
